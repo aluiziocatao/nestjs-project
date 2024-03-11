baab285e1278608aa5850cbd8402bd210f7ffd12
Aluízio Catão <aluizio2210@gmail.com>
2024-03-11 16:51:27 -0500

    Second Commit

---
+++ src/orm-config.ts
@@ -0,0 +1,11 @@
+import { DataSourceOptions } from "typeorm";
+
+export const config: DataSourceOptions = {
+    type: 'sqlite',
+    database: '.db/sql',
+    synchronize: true,
+    logging: true,
+    entities: [__dirname + "/../**/*.entity{.ts,.js}"],
+    migrations: [__dirname + "/../**/*.migration{.ts,.js}"],
+    subscribers: [__dirname + "/../**/*.subscriber{.ts,.js}"],
+}
